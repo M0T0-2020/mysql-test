227251ec5db3a3d5836088497d804865c7606b48
M0T0-2020 <abcdmoto0509@outlook.jp>
2022-10-03 08:17:51 +0900

    add isort and black

--- src/pythondb/connectdb.py
+++ src/pythondb/connectdb.py
@@ -2,26 +2,28 @@ import datetime
 import os
 import random
 from typing import Any, Dict
-from sqlalchemy.engine import Connection as sqlalchemyCon
+
 from sqlalchemy import create_engine, text
+from sqlalchemy.engine import Connection as sqlalchemyCon
 
 dialect = "mysql"
 username = "root"
 # password = os.environ["MYSQLPASSWORD"]
 password = "mysql"
 # hostをlocalhostにするとだめ　(参考):https://qiita.com/hugashy/items/05697f40ae2ccf6a3954
-host="127.0.0.1"
-port='3306'
+host = "127.0.0.1"
+port = "3306"
 database = "mydb"
 
 engine = create_engine(
     f"{dialect}://{username}:{password}@{host}:{port}/{database}?charset=utf8",
-    encoding = "utf-8",
-    echo=False)
+    encoding="utf-8",
+    echo=False,
+)
 print(engine.connect())
 
 
-def create_table(conn:sqlalchemyCon):
+def create_table(conn: sqlalchemyCon):
     conn.execute(text("""DROP TABLE IF EXISTS table01;"""))
     conn.execute(
         text(
@@ -34,19 +36,21 @@ def create_table(conn:sqlalchemyCon):
             """
         )
     )
-    insert_sql:list[str] = []
-    insert_value:Dict[str,Any] = {}
+    insert_sql: list[str] = []
+    insert_value: Dict[str, Any] = {}
     base_date = datetime.datetime.strptime("2022-1-1", "%Y-%m-%d")
     for i in range(2000):
         date = base_date + datetime.timedelta(days=i)
         date_str = date.strftime("%Y-%m-%d")
         insert_sql.append(f"(:col1{i}, :col2{i}, :col3{i}, :col4{i})")
-        insert_value.update({
-            f"col1{i}": f"index{i}",
-            f"col2{i}": date_str,
-            f"col3{i}": i**1/3,
-            f"col4{i}": random.randint(0,100),
-        })
+        insert_value.update(
+            {
+                f"col1{i}": f"index{i}",
+                f"col2{i}": date_str,
+                f"col3{i}": i**1 / 3,
+                f"col4{i}": random.randint(0, 100),
+            }
+        )
     insert_sql = ",".join(insert_sql)
     print(insert_value)
     conn.execute(
@@ -54,8 +58,9 @@ def create_table(conn:sqlalchemyCon):
             f"""
             INSERT INTO `table01` (col1, col2, col3, col4)
             VALUES {insert_sql};
-            """),
-        insert_value
+            """
+        ),
+        insert_value,
     )
 
     result = conn.execute(
@@ -72,6 +77,11 @@ def create_table(conn:sqlalchemyCon):
         print(row["col2"], type(row["col2"]))
         print(row["col3"], type(row["col3"]))
         print(row["col4"], type(row["col4"]))
+
+
+def _get_groupby(conn: sqlalchemyCon, col: str):
+    pass
+
 
 with engine.begin() as conn:
     create_table(conn=conn)
