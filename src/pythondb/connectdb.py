859aedc5357626506c8fca672219aeec86dc1a1d
M0T0-2020 <abcdmoto0509@outlook.jp>
2022-10-04 22:26:38 +0900

    add

--- src/pythondb/connectdb.py
+++ src/pythondb/connectdb.py
@@ -48,7 +48,7 @@ def create_table(conn: sqlalchemyCon, n_rows: int = 2000):
                 f"col1{i}": f"index{i}",
                 f"col2{i}": date_str,
                 f"col3{i}": i**1 / 3,
-                f"col4{i}": random.randint(0, 100),
+                f"col4{i}": random.randint(0, 16),
             }
         )
     insert_sql = ",".join(insert_sql)
@@ -105,14 +105,16 @@ def _get_groupby_transform_col4(conn: sqlalchemyCon, limit: int):
     result = conn.execute(
         text(
             f"""
-            SELECT * 
-            FROM table01
-            LEFT JOIN (
-                SELECT col4 as `col4`, COUNT(col4) as `count_index`, AVG(col3) as `avg_col3`
-                FROM table01 AS table_tmp
-                GROUP BY `col4`
-            ) 
-            ON table01.col4 =  table_tmp.col4
+            SELECT table01.col4, table02.count_index AS `count_col4`, table02.avg_col3 AS `avg_col3`
+            FROM 
+                table01
+                LEFT JOIN (
+                    SELECT col4 AS `col4`, COUNT(col4) as `count_index`, AVG(col3)/100 as `avg_col3`
+                    FROM table01
+                    GROUP BY col4
+                ) 
+            AS table02
+            ON table01.col4 =  table02.col4
             LIMIT {limit}
             ;
             """
@@ -121,11 +123,14 @@ def _get_groupby_transform_col4(conn: sqlalchemyCon, limit: int):
     rows = result.all()
     print(len(rows))
     print(list(result.keys()))
-    for i, row in enumerate(rows):
-        print(i, row[f"col4"], row[f"count_index"], row["avg_col3"])
+    try:
+        for i, row in enumerate(rows):
+            print(i, row[f"col4"], row[f"count_col4"], row[f"avg_col3"])
+    except:
+        pass
 
 
 with engine.begin() as conn:
     create_table(conn=conn)
     _get_groupby_col4(conn=conn, limit=10)
-    _get_groupby_transform_col4(conn=conn, limit=10)
+    _get_groupby_transform_col4(conn=conn, limit=30)
